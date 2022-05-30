651a58bb6e47a1762ce7bf6dd27fb6175b7dcb6b
Janis Ringli <janis.ringli@gmail.com>
2022-05-30 11:35:36 +0200

    create heart symbole with text in it

--- src/main.py
+++ src/main.py
@@ -1,10 +1,70 @@
+import turtle
 from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(250)
-    left(200)
-    if abs(pos()) < 1:
-        break
-end_fill()
+pen = turtle.Turtle()
+
+
+# Defining a method to draw curve
+def curve():
+    for i in range(400):
+        # Defining step by step curve motion
+        pen.right(0.5)
+        pen.forward(0.5)
+
+
+# Defining method to draw a full heart
+def heart():
+    # Set the fill color to red
+    pen.fillcolor('red')
+
+    # Start filling the color
+    pen.begin_fill()
+
+    # Draw the left line
+    pen.left(140)
+    pen.forward(113)
+
+    # Draw the left curve
+    curve()
+    pen.left(120)
+
+    # Draw the right curve
+    curve()
+
+    # Draw the right line
+    pen.forward(112)
+
+    # Ending the filling of the color
+    pen.end_fill()
+
+
+# Defining method to write text
+def txt():
+    # Move turtle to air
+    pen.up()
+
+    # Move turtle to a given position
+    pen.setpos(-68, 95)
+
+    # Move the turtle to the ground
+    pen.down()
+
+    # Set the text color to lightgreen
+    pen.color('white')
+
+    # Write the specified text in
+    # specified font style and size
+    pen.write("You are my world <3", font=(
+        "Verdana", 12, "bold"))
+
+
+# Draw a heart
+heart()
+
+# Write text
+txt()
+
+# To hide turtle
+pen.ht()
 done()
+
+
